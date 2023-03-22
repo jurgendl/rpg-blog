738f6b89a2a4e804ce5ef46b1f8d0030e49b094d
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2023-03-22 22:20:51 +0100

    rpg-blog

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bootstrap-angular'`, () => {
+  it(`should have as title 'rpg-blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bootstrap-angular');
+    expect(app.title).toEqual('rpg-blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bootstrap-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rpg-blog app is running!');
   });
 });
